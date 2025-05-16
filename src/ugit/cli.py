ef32afd217028047c6a23b514834df5b9ad2595e
Gabriel Fortuny <92552031+gabrielfortuny@users.noreply.github.com>
2025-05-15 22:29:36 -0400

    hash object

--- src/ugit/cli.py
+++ src/ugit/cli.py
@@ -1,16 +1,35 @@
 """Command-line interface for ugit."""
 
+from pathlib import Path
+
 import click
 
 from . import data
 
 
 @click.group()
-def cli():
+def cli() -> None:
     """ugit CLI."""
 
 
 @cli.command()
-def init():
+def init() -> None:
     """Initialize a new repository."""
     data.init()
+
+
+@cli.command()
+@click.argument("filename", type=click.Path(exists=True, dir_okay=False, readable=True))
+def hash_object(filename: str) -> None:
+    """
+    Hash and store the contents of a file.
+
+    Args:
+        filename (str): Path to the file to hash.
+    """
+    if not Path(".ugit").is_dir():
+        click.echo(
+            "Error: No .ugit repository found in the current directory.", err=True
+        )
+    with open(filename, "rb") as file:
+        data.hash_object(file.read())
--- src/ugit/data.py
+++ src/ugit/data.py
@@ -1,18 +1,37 @@
 """Core data operations."""
 
+from hashlib import sha256
 from pathlib import Path
 
+REPO_PATH = Path(".ugit")
+OBJECTS_PATH = REPO_PATH / "objects"
 
-def init():
+
+def init() -> None:
     """Initialize a new repository."""
-    repo_path = Path(".ugit")
-    objects_path = repo_path / "objects"
     try:
-        repo_path.mkdir()
-        print(f"Directory '{repo_path}' created successfully.")
-        objects_path.mkdir()
-        print(f"Directory '{objects_path}' created successfully.")
+        REPO_PATH.mkdir()
+        print(f"Directory '{REPO_PATH}' created successfully.")
+        OBJECTS_PATH.mkdir()
+        print(f"Directory '{OBJECTS_PATH}' created successfully.")
     except FileExistsError as e:
         print(f"Directory or file '{e.filename}' already exists.")
     except PermissionError as e:
         print(f"Permission denied: Unable to create '{e.filename}'.")
+
+
+def hash_object(data: bytes) -> None:
+    """
+    Hashes the given data using SHA-256 and stores it as an object.
+
+    Args:
+        data (str): The data to be hashed and stored.
+
+    Returns:
+        str: The SHA-256 hexadecimal hash of the data, used as the object ID.
+    """
+    object_id = sha256(data).hexdigest()
+    object_path = OBJECTS_PATH / object_id
+    with open(object_path, "wb") as out:
+        out.write(data)
+    print(f"Hashed object id: {object_id}")
